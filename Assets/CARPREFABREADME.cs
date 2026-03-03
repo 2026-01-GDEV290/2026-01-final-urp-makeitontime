0f718e130dbfb5c041f02e3474abb8791834970f
FourRVCC30 <179720166+4RVCC30@users.noreply.github.com>
2026-03-03 14:00:28 -0500

    Made the car an exportable unityPackage
    I gotta do some work with collectables using the car script and I'd rather do it on it's own branch. That being said I still need the carOBJ to do anything. This just makes it easier

---
+++ Assets/CARPREFABREADME.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class CARPREFABREADME : MonoBehaviour
+{
+    /*
+     * Hey you! if u got any questions as to why the car prefab isn't working I'll write down possible reasons here as I find them
+     * I'm not sure if this is the proper way to do something like this but for the sake of it this is how I'll be doing it
+     * 
+     * - Make sure that you define the "Ground Layer" in the CarScript component on the CarOBJ object
+     */
+}
