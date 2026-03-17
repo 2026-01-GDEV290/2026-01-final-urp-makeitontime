d4aa0312fddff96826c244a2a8999a901122d0b4
FourRVCC30 <179720166+4RVCC30@users.noreply.github.com>
2026-03-17 09:32:51 -0400

    added cop sfx

--- Assets/CopAI.cs
+++ Assets/CopAI.cs
@@ -20,7 +20,7 @@ public class CopAI : MonoBehaviour
     private void Update()
     {
         float distance = Vector3.Distance(transform.position,playerTransform.position);
-        if (distance < 50)
+        if (distance < 75)
         {
             agent.SetDestination(playerTransform.position);
         }else if(remapPath)
