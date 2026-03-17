b7ead3131beeb05fdd82adcfc671037104c5b693
FourRVCC30 <179720166+4RVCC30@users.noreply.github.com>
2026-03-17 10:04:41 -0400

    added a short delay before game start

--- Assets/GameLoadStart.cs
+++ Assets/GameLoadStart.cs
@@ -1,9 +1,20 @@
+using System.Collections;
 using UnityEngine;
 
 public class GameLoadStart : MonoBehaviour
 {
+    LoadingScreenScript loadingScreenScript;
     private void Awake()
     {
-        GetComponent<LoadingScreenScript>().LoadLevel(1);          
+        loadingScreenScript = GetComponent<LoadingScreenScript>();
+    }
+    private void Start()
+    {
+        StartCoroutine(gameStart());
+    }
+    IEnumerator gameStart()
+    {
+        yield return new WaitForSeconds(1);
+            loadingScreenScript.LoadLevel(1); 
     }
 }
