d6ffd757ccff953237460428033eea9b450b5fa7
FourRVCC30 <179720166+4RVCC30@users.noreply.github.com>
2026-04-29 18:47:14 -0400

    added blowy intro anim and fixed up start screen

--- Assets/_Scripts/GameOverScript.cs
+++ Assets/_Scripts/GameOverScript.cs
@@ -1,18 +1,40 @@
+using System.Collections;
 using UnityEngine;
 
 public class GameOverScript : MonoBehaviour
 {
     [SerializeField]
     GameObject gameOverUI;
+    [SerializeField]
+    Animator anim;
+    [SerializeField]
+    AudioSource bark;
+    [SerializeField]
+    AudioSource bars;
+
+    bool gameOver = false;
 
     private void Awake()
     {
         Time.timeScale = 1;
         gameOverUI.SetActive(false);
+        bars.Play();
     }
     public void GameOver()
     {
-        Time.timeScale = 0f;
-        gameOverUI.SetActive(true);
+        if (gameOver == false)
+        {
+            Time.timeScale = 0f;
+            gameOverUI.SetActive(true);
+            anim.Play("GameOver");
+            StartCoroutine(PlayBark());
+            gameOver = true;
+        }
+    }
+
+    IEnumerator PlayBark()
+    {
+        yield return new WaitUntil(() => anim.GetCurrentAnimatorStateInfo(0).IsName("PlayBark"));
+        bark.Play();
     }
 }
