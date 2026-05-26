b028f721fc8de1a2bf4a223bb1d253bf54ae9091
4RVCC30 <G00300532@stu.raritanval.edu>
2026-05-25 20:20:55 -0400

    added the intercept feature (needs to be tested further)

--- Assets/_Scripts/CopAI.cs
+++ Assets/_Scripts/CopAI.cs
@@ -16,7 +16,16 @@ public class CopAI : MonoBehaviour
     GameObject child;
     [SerializeField]
     AudioSource childSource;
+
+    [SerializeField]
+    Transform goal;
     public bool destroyed;
+
+    enum State {chasing, intercept, waiting};
+
+    private float waitTime;
+    [SerializeField]
+    private State currentState;
 
     private void Awake()
     {
@@ -27,6 +36,16 @@ public class CopAI : MonoBehaviour
     private void Start()
     {
         playerTransform = FindFirstObjectByType<CartScript>().gameObject.transform;
+
+        if(FindFirstObjectByType<GoalScript>() != null)
+        {
+            goal = FindFirstObjectByType<GoalScript>().gameObject.transform;
+        }else
+        {
+            Debug.LogError("No goal found in scene");
+            goal = transform;
+        }
+
        // child = GetComponentInChildren<GameObject>();
         //childSource = child.GetComponent<AudioSource>();
     }
@@ -36,7 +55,11 @@ public class CopAI : MonoBehaviour
         {
             return;
         }
-        float distance = Vector3.Distance(transform.position,playerTransform.position);
+
+        setState();
+        stateLogic();
+
+       /* float distance = Vector3.Distance(transform.position,playerTransform.position);
         if (distance < 75 && agent.enabled == true)
         {
             agent.SetDestination(playerTransform.position);
@@ -45,13 +68,71 @@ public class CopAI : MonoBehaviour
             remapPath = false;
             agent.SetDestination(playerTransform.position);
             StartCoroutine(repath());
-        }
+        }*/
+
         if (destroyed && !childSource.isPlaying)
         {
             destroyed = false;
             agent.enabled = false;
             gameObject.SetActive(false);
         }
+    }
+
+    private void setState()
+    {
+        float distanceFromPlayer = Vector3.Distance(transform.position, playerTransform.position);
+        float distanceFromTarget = Vector3.Distance(transform.position, agent.destination);
+
+        if(distanceFromTarget < 5 && agent.enabled == true && currentState != State.waiting)
+        {
+            currentState = State.waiting;
+            waitTime = Random.Range(5, 10);
+        }
+        if(distanceFromTarget < 5 && agent.enabled == true)
+        {
+            currentState = State.waiting;
+        }
+        if(distanceFromPlayer > 200 && agent.enabled == true && currentState != State.intercept)
+        {
+            currentState = State.waiting;
+            setInterceptPoint();
+        }
+        if(distanceFromPlayer > 200 && agent.enabled == true && currentState != State.waiting)
+        {
+            currentState = State.intercept;
+        }
+        if(distanceFromPlayer < 200 && agent.enabled == true)
+        {
+            currentState = State.chasing;
+        }
+    }
+
+    private void stateLogic()
+    {
+        switch(currentState)
+        {
+            case State.chasing:
+                agent.SetDestination(playerTransform.position);
+                break;
+            case State.intercept:
+                
+                break;
+            case State.waiting:
+                waitTime -= Time.deltaTime;
+                if(waitTime < 0)
+                {
+                    currentState = State.intercept;
+                }
+                break;
+        }
+    }
+
+    private void setInterceptPoint()
+    {
+        Vector3 middlePoint = (playerTransform.position + goal.position) / 2;
+        Vector3 direction = (middlePoint - transform.position).normalized;
+        Vector3 interceptPoint = middlePoint + direction * 10;
+        agent.SetDestination(interceptPoint);
     }
     public void DestroyThisCar()
     {
