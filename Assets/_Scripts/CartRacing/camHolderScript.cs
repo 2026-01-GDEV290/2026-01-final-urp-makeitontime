d01522fbcdb15df0447437d831b8239d70095d18
4RVCC30 <G00300532@stu.raritanval.edu>
2026-07-16 15:59:30 -0400

    Fixed the camera on the car prefab and made sure that all audio sources were in the proper mixers

--- Assets/_Scripts/CartRacing/camHolderScript.cs
+++ Assets/_Scripts/CartRacing/camHolderScript.cs
@@ -4,9 +4,12 @@ public class camHolderScript : MonoBehaviour
 {
     [SerializeField]
     Transform carPos;
+    void Start()
+    {
+        transform.localPosition = new Vector3(0,-1,0);
+    }
     void Update()
     {
-        transform.localPosition = 
         // follow the car's pos but only y rotation
         // rigidbodies get kinda weird when a cam is attached so this keeps the camera nice and smooth
         transform.position = carPos.position;
